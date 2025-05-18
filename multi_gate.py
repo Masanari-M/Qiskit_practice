035acaad54952b4dba40105cc7060dc2dccdaa17
Masanari Miyamoto <u957000h@ecs.osaka-u.ac.jp>
2025-05-18 18:23:10 +0900

    initial commit from mac

---
+++ multi_gate.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[3]:
+
+import qiskit
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+from numpy import pi
+import numpy as np
+
+def qft_rotations(circuit, n):
+    """Performs qft on the first n qubits in circuit (without swaps)"""
+    if n == 0:
+        return circuit
+    n -= 1
+    circuit.h(n)
+    for qubit in range(n):
+        circuit.cp(pi/2**(n-qubit), qubit, n)
+    # At the end of our function, we call the same function again on
+    # the next qubits (we reduced n by one earlier in the function)
+    qft_rotations(circuit, n)
+
+
+
+# In[ ]:
+
+
+
